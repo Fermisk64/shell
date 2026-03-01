2aa7065bc598e41d6c621f3e7a22c6773be1b71e
Fermisk Saleh <fermisk.x@gmail.com>
2026-03-01 14:32:28 -0500

    Assignment updated

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -42,11 +42,11 @@ mkdir -p ./data/processed
 mkdir -p ./data/processed/server_logs ./data/processed/user_logs ./data/processed/event_logs
 
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
-Cp ./data/raw/*server*.log ./data/processed/server_logs
+Cp ./data/raw/*server*.log ./data/processed/server_logs/
 
 # 6. Repeat the above step for user logs and event logs
-Cp ./data/raw/*user*.log ./data/processed/user_logs
-Cp ./data/raw/*event*.log ./data/processed/event_logs
+Cp ./data/raw/*user*.log ./data/processed/user_logs/
+Cp ./data/raw/*event*.log ./data/processed/event_logs/
 
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
 rm ./data/raw/*ipaddr* ./data/processed/user_logs/*ipaddr*
